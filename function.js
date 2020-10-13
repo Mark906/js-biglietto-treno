b3ea9c62d70f7bd201e48e59af5d6c2448899b77
Marco <markoka1@outlook.it>
2020-10-13 16:02:21 +0200

    seconda versione del codice

--- function.js
+++ function.js
@@ -1,11 +1,14 @@
 var km = prompt('quanti km devi percorrere?');
-var eta = prompt('che età hai?');
+//var eta = prompt('quanti anni hai?');
 
+//questa è una versione di codice che rispecchia l'ottica -
+//del risparmio delle variabili in quanto fondamentale in ambienti piu complessi -
+//dove un grande numero di variabili superflue comportano rallentamenti nei caricamenti dei contenuti.
 var prezzo = 0.21 * km;
 
-if (eta < 18) {
+if (prompt('quanti anni hai?') < 18) {
     prezzo = prezzo - (prezzo * (20/100));
-} else if (eta > 65) {
+} else if (prompt('quanti anni hai?') > 65) {
     prezzo = prezzo - (prezzo * (40/100));
 }
 
